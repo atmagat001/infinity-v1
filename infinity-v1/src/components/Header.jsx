6bf09a071329d9e231ed8019d46ce08453f0a9a3
atmagat001 <atmagat@gmail.com>
2023-06-16 08:08:14 +0800

    commit now

--- infinity-v1/src/components/Header.jsx
+++ infinity-v1/src/components/Header.jsx
@@ -21,16 +21,18 @@ function Header() {
                 nav ? "nav-container nav-active" : "nav-container"
             }>
                 <ul>
-                    <li><a href="">About Us</a></li>
-                    <li><a href="">Our Services</a></li>
-                    <li><a href="">Data Sets</a></li>
-                    <li><a href="">Data Pro</a></li>
-                    <li><a href="">Contact Us</a></li>
+                    <li><a href="/#">About Us</a></li>
+                    <li><a href="/#">Our Services</a></li>
+                    <li><a href="/#">Data Sets</a></li>
+                    <li><a href="/#">Data Pro</a></li>
+                    <li><a href="/#">Contact Us</a></li>
                 </ul>
                 
             </div>
             <button onClick={handleNav}><i class="fa-solid fa-bars"></i></button>
         </header>
+
+        
     </div>
   )
 }
